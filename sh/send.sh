01d9baef4563a4ff2bddecc625ee42d961c1b808
Victor Avendano <avenda@gmail.com>
2016-01-10 23:42:16 -0300

    added VARS

--- sh/send.sh
+++ sh/send.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+DCM_VERSION=3.3.7
 AET=$1
 HOST=$2
 PORT=$3
 DICOM_DIR=$4
+#if binaries arent downloaded
+if [ ! -d dcm4che-$DCM_VERSION ]; then
+    wget http://tcpdiag.dl.sourceforge.net/project/dcm4che/dcm4che3/$DCM_VERSION/dcm4che-$DCM_VERSION-bin.zip
+    unzip dcm4che-$DCM_VERSION-bin.zip
+fi
+cd dcm4che-$DCM_VERSION && bin/storescu -c $1@$2:$3 $4
 
-#download binaries
-wget http://tcpdiag.dl.sourceforge.net/project/dcm4che/dcm4che3/3.3.7/dcm4che-3.3.7-bin.zip
-unzip dcm4che-3.3.7-bin.zip
-cd dcm4che-3.3.7
-bin/storescu -c $1@$2:$3 $4
-#run dcmstr with params (valdate it before)
